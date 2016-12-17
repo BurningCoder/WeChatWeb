e58c80db56bc57388274a526eee8c5058f393d3f
Chausson <chausson@ChaussondeMacBook-Pro.local>
2016-12-17 18:18:23 +0800

    更改小程序样式

--- pages/film/film.js
+++ pages/film/film.js
@@ -5,19 +5,19 @@ Page({
       {date:"2016年11月20日 星期五  今天",
       items:[
         {name:"杀破狼",imageUrl:"http://img3.cache.netease.com/photo/0003/2015-05-28/AQNGN78800B50003.jpg"
-        ,scores:"6.8",id:"123123"
+        ,scores:"6.8",detailScores:"豆瓣:6.8   IMDB:9.0   烂番茄:7.6",id:"123123"
         },
          {name:"复仇者联盟2",imageUrl:"http://korea.people.com.cn/mediafile/201403/30/F201403301346181540326346.jpg"
-        ,scores:"9.8",id:"123123"
+        ,scores:"9.8",detailScores:"豆瓣:6.8   IMDB:9.0   烂番茄:7.6",id:"123123"
         },
         ]
       },
       {date:"2016年11月18日 星期三  ",
       items:[
         {name:"我不是潘金莲",imageUrl:"http://uploads.yjbys.com/allimg/201611/24-1611191F6222L.png",
-        scores:"7.8",id:"123123"},
+        scores:"7.8",detailScores:"豆瓣:6.8   IMDB:9.0   烂番茄:7.6",id:"123123"},
         {name:"奇异博士",imageUrl:"http://img.52fuqing.com/upload/editor/2016-11-6/2016116205224938vycic.jpg",
-        scores:"8.8",id:"123123"}
+        scores:"8.8",detailScores:"豆瓣:6.8   IMDB:9.0   烂番茄:7.6",id:"123123"}
         ]
       }
      
